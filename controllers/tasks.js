81459c80b6df431f104a0f9a503c4e6d9953f0f0
Alejandra De Los Santos <aledeloss@gmail.com>
2023-11-18 12:07:18 -0300

    Add controller with basic logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,49 @@
-const express = require('express');
-
 let tasks = [
   {
     id: 1,
-    description: 'Home test',
+    title: 'Home test',
+    description: 'long description',
   },
 ];
 
+// TODO: Handle errors more accurately
 // Returns all stored tasks.
 const getAllTasks = async (req, res) => {
-  res.status(200).json(tasks);
+  try {
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
 };
 
-module.exports = { getAllTasks };
+// Add a new task
+const addTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    // TODO:Validate if task already exists and no empty title
+    const newTask = {
+      title,
+      description,
+      id: 2, // TODO: Add a unique id
+    };
+    tasks.push(newTask);
+    res.status(201).json(tasks);
+  } catch (e) {
+    res.status(500).json({ error: e, message: 'Something went wrong' });
+  }
+};
+
+// Remove task by id.
+const removeTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const toDeleteId = Number(id);
+    // TODO: Validate that the task exists
+    tasks = tasks.filter((task) => task.id !== id);
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
+module.exports = { addTask, getAllTasks, removeTaskById };
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const { getAllTasks } = require('../controllers/tasks');
+const {
+  addTask,
+  getAllTasks,
+  removeTaskById,
+} = require('../controllers/tasks');
 
 /* List all tasks. */
 router.get('/tasks', getAllTasks);
 
 /* Add a new task. */
-router.post('/tasks', function (req, res, next) {
-  res.status(200).json({ response: 'Well done post' });
-});
+router.post('/tasks', addTask);
 
 /* Delete a task by ID. */
-router.delete('/tasks', function (req, res, next) {
-  res.status(200).json({ response: 'Well done delete' });
-});
+router.delete('/tasks/:id', removeTaskById);
 
 module.exports = router;
