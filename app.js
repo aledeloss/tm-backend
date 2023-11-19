d6e66f4b264cc6d41240ce570789e60989c10c6f
Alejandra De Los Santos <aledeloss@gmail.com>
2023-11-19 09:13:12 -0300

    Fix redirect

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 
 const app = express();
-
+app.disable('etag');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
